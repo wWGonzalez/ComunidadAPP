3f9e4f278e66f0f81d7b2ed54324b6e7e9b5e5a3
JhoseD <32523459+JhoseD@users.noreply.github.com>
2018-05-21 17:04:18 -0600

    clase para llamar about en frame

---
+++ app/src/main/java/umg/community/app/com/communityumg/interfaces/about2.java
@@ -0,0 +1,15 @@
+package umg.community.app.com.communityumg.interfaces;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import umg.community.app.com.communityumg.R;
+
+public class about2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about2);
+    }
+}
