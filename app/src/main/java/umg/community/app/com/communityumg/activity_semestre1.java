bfda75c79b3fa70e496b6ad203e19b9a8ec5c6bd
JhoseD <32523459+JhoseD@users.noreply.github.com>
2018-05-22 00:26:27 -0600

    formulario publicar, 10 semestres

---
+++ app/src/main/java/umg/community/app/com/communityumg/activity_semestre1.java
@@ -0,0 +1,13 @@
+package umg.community.app.com.communityumg;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_semestre1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_semestre1);
+    }
+}
